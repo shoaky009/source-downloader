bed2379b7fb43debb9261bc7edf5990950367574
shoaky <v_shoaky@outlook.com>
2023-08-23 21:57:35 +0800

    test: 修复Windows下出现的路径错误

--- plugins/source-downloader-common-plugin/src/test/kotlin/io/github/shoaky/sourcedownloader/common/anime/AnimeTaggerTest.kt
+++ plugins/source-downloader-common-plugin/src/test/kotlin/io/github/shoaky/sourcedownloader/common/anime/AnimeTaggerTest.kt
@@ -13,7 +13,7 @@ class AnimeTaggerTest {
         val f0 = FixedFileContent(fileDownloadPath = Path("[ANi] Dark Gathering -  黑暗集会 - 01 [1080P][Baha][WEB-DL][AAC AVC][CHT][MP4].mp4"))
 
         assertEquals(null, AnimeTagger.tag(SourceFile(f0.fileDownloadPath)))
-        val f1 = FixedFileContent(fileDownloadPath = Path("[爱恋字幕社&漫猫字幕社][剧场版关于我转生变成史莱姆这档事 红莲之绊篇][Tensei shitara Slime Datta Ken Movie: Guren no Kizuna-hen][1080p][MP4][简中]"))
+        val f1 = FixedFileContent(fileDownloadPath = Path("[爱恋字幕社&漫猫字幕社][剧场版关于我转生变成史莱姆这档事 红莲之绊篇][Tensei shitara Slime Datta Ken Movie Guren no Kizuna-hen][1080p][MP4][简中]"))
         assertEquals("movie", AnimeTagger.tag(SourceFile(f1.fileDownloadPath)))
         val f2 = FixedFileContent(fileDownloadPath = Path("LoveLive! 虹咲学园学园偶像同好会 Next Sky OVA GB_CN HEVC10_opus 1080p [430.13 MB]"))
         assertEquals("ova", AnimeTagger.tag(SourceFile(f2.fileDownloadPath)))
