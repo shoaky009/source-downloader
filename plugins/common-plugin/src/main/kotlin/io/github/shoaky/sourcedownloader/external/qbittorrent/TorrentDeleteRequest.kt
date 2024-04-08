3313aab144fc5d239ebd64b46c6741d06b24be24
shoaky <v_shoaky@outlook.com>
2024-04-09 00:33:52 +0800

    fix: qbittorrent错误的请求参数

--- plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/qbittorrent/TorrentDeleteRequest.kt
+++ plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/qbittorrent/TorrentDeleteRequest.kt
@@ -1,12 +1,16 @@
 package io.github.shoaky.sourcedownloader.external.qbittorrent
 
 import com.fasterxml.jackson.core.type.TypeReference
+import com.google.common.net.MediaType
+import io.github.shoaky.sourcedownloader.sdk.http.HttpMethod
 
 class TorrentDeleteRequest(
-    hashes: List<String>,
-    deleteFiles: Boolean = false
+    val hashes: List<String>,
+    val deleteFiles: Boolean = false
 ) : QbittorrentRequest<String>() {
 
-    override val path: String = "/api/v2/torrents/delete?hashes=${hashes.joinToString("|")}&deleteFiles=$deleteFiles"
+    override val path: String = "/api/v2/torrents/delete"
     override val responseBodyType: TypeReference<String> = stringTypeReference
+    override val httpMethod: String = HttpMethod.POST.name
+    override val mediaType: MediaType = MediaType.FORM_DATA
 }
