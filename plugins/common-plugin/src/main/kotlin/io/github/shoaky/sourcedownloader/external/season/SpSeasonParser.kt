b48048943be8aee5ee3e16f2b1db4b360fb794bd
shoaky <v_shoaky@outlook.com>
2024-04-12 22:14:54 +0800

    feat: 增强特别篇识别

--- plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/season/SpSeasonParser.kt
+++ plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/season/SpSeasonParser.kt
@@ -5,7 +5,7 @@ object SpSeasonParser : SeasonParser {
     private val spRegexes = listOf(
         "OVA|OAD|SPs|S00|SP(\\d+)|SP (\\d+)".toRegex(),
         "Special|extra(\\d+)".toRegex(RegexOption.IGNORE_CASE),
-        "特别篇|\\[SP]|映像特典".toRegex(),
+        "特别篇|特別篇|\\[SP]|映像特典".toRegex(),
     )
 
     override fun input(subject: String): SeasonResult? {
