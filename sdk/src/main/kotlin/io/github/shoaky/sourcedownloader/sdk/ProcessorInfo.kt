03c7de3c6ebcf8e2c66a9fb89b71fb5ddd7138ab
shoaky <v_shoaky@outlook.com>
2024-04-17 15:49:35 +0800

    feat: 调整Path序列化方式

--- sdk/src/main/kotlin/io/github/shoaky/sourcedownloader/sdk/ProcessorInfo.kt
+++ sdk/src/main/kotlin/io/github/shoaky/sourcedownloader/sdk/ProcessorInfo.kt
@@ -1,10 +1,14 @@
 package io.github.shoaky.sourcedownloader.sdk
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import java.nio.file.Path
 
 data class ProcessorInfo(
     val name: String,
+    @JsonSerialize(using = ToStringSerializer::class)
     val downloadPath: Path,
+    @JsonSerialize(using = ToStringSerializer::class)
     val sourceSavePath: Path,
     val tags: List<String> = emptyList(),
     val category: String? = null,
