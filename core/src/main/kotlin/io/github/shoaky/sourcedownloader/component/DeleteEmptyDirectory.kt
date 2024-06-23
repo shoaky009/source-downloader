966631094020e798dffb2c97b8f3d003b5443960
shoaky <v_shoaky@outlook.com>
2024-06-23 19:23:08 +0800

    feat: 单文件支持删除文件夹

--- core/src/main/kotlin/io/github/shoaky/sourcedownloader/component/DeleteEmptyDirectory.kt
+++ core/src/main/kotlin/io/github/shoaky/sourcedownloader/component/DeleteEmptyDirectory.kt
@@ -3,6 +3,7 @@ package io.github.shoaky.sourcedownloader.component
 import io.github.shoaky.sourcedownloader.sdk.ItemContent
 import io.github.shoaky.sourcedownloader.sdk.ProcessContext
 import io.github.shoaky.sourcedownloader.sdk.component.ProcessListener
+import java.nio.file.Files
 import kotlin.io.path.*
 
 /**
@@ -12,6 +13,16 @@ object DeleteEmptyDirectory : ProcessListener {
 
     @OptIn(ExperimentalPathApi::class)
     override fun onItemSuccess(context: ProcessContext, itemContent: ItemContent) {
+        if (singleFile(itemContent)) {
+            val fileContent = itemContent.fileContents.first()
+            val parent = fileContent.fileDownloadPath.parent
+            val isEmpty = Files.newDirectoryStream(parent).use { it.none() }
+            if (isEmpty) {
+                parent.deleteIfExists()
+            }
+            return
+        }
+
         itemContent.fileContents.firstOrNull()?.run {
             val directoryPath = this.fileDownloadRootDirectory()
             if (directoryPath != null && directoryPath.walk(PathWalkOption.INCLUDE_DIRECTORIES)
@@ -21,4 +32,6 @@ object DeleteEmptyDirectory : ProcessListener {
             }
         }
     }
+
+    private fun singleFile(itemContent: ItemContent) = itemContent.fileContents.size == 1
 }
