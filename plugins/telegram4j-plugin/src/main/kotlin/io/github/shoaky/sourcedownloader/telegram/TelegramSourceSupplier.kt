4361899dcae4550cb3df8e5b1b652e485efd60d7
shoaky <v_shoaky@outlook.com>
2024-07-04 17:42:31 +0800

    fix: telegram错误的声明

--- plugins/telegram4j-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/telegram/TelegramSourceSupplier.kt
+++ plugins/telegram4j-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/telegram/TelegramSourceSupplier.kt
@@ -2,7 +2,6 @@ package io.github.shoaky.sourcedownloader.telegram
 
 import io.github.shoaky.sourcedownloader.sdk.CoreContext
 import io.github.shoaky.sourcedownloader.sdk.Properties
-import io.github.shoaky.sourcedownloader.sdk.component.ComponentRule
 import io.github.shoaky.sourcedownloader.sdk.component.ComponentSupplier
 import io.github.shoaky.sourcedownloader.sdk.component.ComponentType
 import io.github.shoaky.sourcedownloader.sdk.plugin.PluginContext
@@ -23,12 +22,6 @@ class TelegramSourceSupplier(
     override fun supplyTypes(): List<ComponentType> {
         return listOf(
             ComponentType.source("telegram"),
-        )
-    }
-
-    override fun rules(): List<ComponentRule> {
-        return listOf(
-            ComponentRule.allowDownloader(TelegramSource::class),
         )
     }
 
