dd93b6100c768cc19b98fc4ae625ac4e3f42cba0
shoaky <v_shoaky@outlook.com>
2024-10-25 10:07:08 +0800

    chore: 调整过时的API

--- plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/rss/ItemExt.kt
+++ plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/rss/ItemExt.kt
@@ -1,8 +1,9 @@
 package io.github.shoaky.sourcedownloader.external.rss
 
+import com.apptasticsoftware.rssreader.DateTimeParser
 import com.apptasticsoftware.rssreader.Item
 
-class ItemExt : Item() {
+class ItemExt(dateTimeParser: DateTimeParser) : Item(dateTimeParser) {
 
     val tags: MutableSet<String> = mutableSetOf()
     val attrs: MutableMap<String, Any> = mutableMapOf()
--- plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/rss/RssExtReader.kt
+++ plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/rss/RssExtReader.kt
@@ -7,12 +7,12 @@ import io.github.shoaky.sourcedownloader.sdk.util.http.httpClient
 
 class RssExtReader : AbstractRssReader<Channel, ItemExt>(httpClient) {
 
-    override fun createChannel(dateTimeParser: DateTimeParser?): Channel {
+    override fun createChannel(dateTimeParser: DateTimeParser): Channel {
         return Channel(dateTimeParser)
     }
 
-    override fun createItem(dateTimeParser: DateTimeParser?): ItemExt {
-        return ItemExt()
+    override fun createItem(dateTimeParser: DateTimeParser): ItemExt {
+        return ItemExt(dateTimeParser)
     }
 
 }
