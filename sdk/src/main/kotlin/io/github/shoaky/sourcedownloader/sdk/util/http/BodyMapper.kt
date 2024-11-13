2e6bc077e6c6747a34555ec59602301ec921ef59
shoaky <v_shoaky@outlook.com>
2024-11-13 21:57:39 +0800

    fix: 设置默认连接超时时间

--- sdk/src/main/kotlin/io/github/shoaky/sourcedownloader/sdk/util/http/BodyMapper.kt
+++ sdk/src/main/kotlin/io/github/shoaky/sourcedownloader/sdk/util/http/BodyMapper.kt
@@ -5,10 +5,12 @@ import java.net.CookieManager
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
+import java.time.Duration
 
 val defaultCookieManager = CookieManager()
 val httpClient: HttpClient = HttpClient.newBuilder()
     .cookieHandler(defaultCookieManager)
+    .connectTimeout(Duration.ofSeconds(10))
     .build()
 
 internal val log = LoggerFactory.getLogger("HTTP")
