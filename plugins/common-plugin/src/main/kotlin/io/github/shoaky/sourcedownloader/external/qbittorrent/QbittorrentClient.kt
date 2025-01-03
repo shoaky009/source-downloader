759f67022e17039de2937a82eb3f5311e4dd0a77
shoaky <v_shoaky@outlook.com>
2025-01-03 16:19:22 +0800

    chore: 调整日志

--- plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/qbittorrent/QbittorrentClient.kt
+++ plugins/common-plugin/src/main/kotlin/io/github/shoaky/sourcedownloader/external/qbittorrent/QbittorrentClient.kt
@@ -22,7 +22,7 @@ class QbittorrentClient(
     private fun tryLogin() {
         val sidCookie = getSidCookie()
         if (sidCookie != null && authenticationLegal) {
-            log.debug("sidCookie:${sidCookie.value}")
+            log.debug("Found sid cookie:{}, skip login", sidCookie.value)
             return
         }
 
@@ -37,16 +37,16 @@ class QbittorrentClient(
         log.warn("未找到qBittorrent sid cookie")
     }
 
-    fun <R : BaseRequest<T>, T : Any> execute(request: R): HttpResponse<T> {
-        return super.execute(server, request)
-    }
-
     private fun getSidCookie(): HttpCookie? {
-        val cookies = defaultCookieManager.cookieStore.get(server)
+        val cookies = defaultCookieManager.cookieStore[server]
         if (log.isDebugEnabled) {
             log.debug("qBittorrent cookies:${Jackson.toJsonString(cookies)}")
         }
         return cookies.filter { it.hasExpired().not() }.firstOrNull { it.name == SID_COOKIE_NAME }
+    }
+
+    fun <R : BaseRequest<T>, T : Any> execute(request: R): HttpResponse<T> {
+        return super.execute(server, request)
     }
 
     override fun <R : BaseRequest<T>, T : Any> beforeRequest(requestBuilder: HttpRequest.Builder, request: R) {
@@ -59,12 +59,10 @@ class QbittorrentClient(
     }
 
     override fun <R : BaseRequest<T>, T : Any> afterRequest(response: HttpResponse<T>, request: R) {
-        if (request is QbittorrentRequest<*>) {
-            if (request.authenticationRequired && response.statusCode() == 403) {
-                authenticationLegal = false
-                log.info("认证非法尝试重新登陆qBittorrent...")
-                tryLogin()
-            }
+        if (request is QbittorrentRequest<*> && request.authenticationRequired && response.statusCode() == 403) {
+            authenticationLegal = false
+            log.info("认证非法尝试重新登陆qBittorrent...")
+            tryLogin()
         }
         if (response.statusCode() != 200) {
             log.warn(
